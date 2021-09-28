aba65f5f1fb806a9df34545b2526911783852aa5
Johann <johann.de-almeida@lacatholille.fr>
2021-09-28 16:52:14 +0200

    :sparkles: Correction boutons filtre pour utiliser les chips et introduction à la recherche

--- lib/main.dart
+++ lib/main.dart
@@ -221,15 +221,39 @@ class Accueil extends StatelessWidget {
   }
 }
 
-class Recherche extends StatelessWidget {
+class Recherche extends StatefulWidget {
+  const Recherche({Key? key}) : super(key: key);
+
+  @override
+  _RechercheState createState() => _RechercheState();
+}
+
+class _RechercheState extends State<Recherche> {
+  bool _isStartersOn = false;
+  bool _isDishesOn = false;
+  bool _isDesertsOn = false;
+
+  bool _isSearchEmpty = true;
+
+  final duplicateItems = List<String>.generate(10, (i) => "Item $i");
+
+  void filterSearchResults(String query) {
+    setState(() {
+      _isSearchEmpty = query.isEmpty;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: SingleChildScrollView(child:Column(children: [
+        child: Column(children: [
       Padding(
         padding: const EdgeInsets.all(8.0),
         child: TextField(
           //controller: editingController,
+          onChanged: (value) {
+            filterSearchResults(value);
+          },
           decoration: InputDecoration(
               labelText: "Search",
               hintText: "Search",
@@ -238,43 +262,111 @@ class Recherche extends StatelessWidget {
                   borderRadius: BorderRadius.all(Radius.circular(25.0)))),
         ),
       ),
-    Padding(
-    padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
-    child:Row(children:[
-    Padding(
-    padding: const EdgeInsets.only(right: 8.0),
-    child:TextButton(onPressed: () {}, child: Text("Entrée"),style: ButtonStyle(
-            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(18.0),
-                    side: BorderSide(color: Colors.red)
-                )
-            )
-        )),),
-    Padding(
-    padding: const EdgeInsets.only(right: 8.0),
-    child:TextButton(onPressed: () {}, child: Text("Plat"),style: ButtonStyle(
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
-                  side: BorderSide(color: Colors.red)
-              )
-          )
-      )),),
-    Padding(
-    padding: const EdgeInsets.only(right: 8.0),
-    child:TextButton(onPressed: () {}, child: Text("Dessert"),style: ButtonStyle(
-          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-              RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
-                  side: BorderSide(color: Colors.red)
-              )
-          )
-      )),),
-      ]),),
-      //Text("Recherche"),
-      Lottie.asset("36895-healthy-or-junk-food.json")
-    ])));
+      Padding(
+          padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 0),
+          child: Row(children: [
+            ActionChip(
+              elevation: 8.0,
+              padding: EdgeInsets.all(2.0),
+              avatar: CircleAvatar(
+                backgroundColor: Colors.blueAccent,
+                child: Icon(
+                  _isStartersOn
+                      ? Icons.bakery_dining_rounded
+                      : Icons.bakery_dining_outlined,
+                  color: Colors.white,
+                  size: 20,
+                ),
+              ),
+              label: Text('Entrées'),
+              onPressed: () {
+                setState(() {
+                  _isStartersOn = !_isStartersOn;
+                });
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text('Entrées')));
+              },
+              backgroundColor: Colors.grey[200],
+              shape: StadiumBorder(
+                  side: BorderSide(
+                width: 1,
+                color: Colors.redAccent,
+              )),
+            ),
+            ActionChip(
+              elevation: 8.0,
+              padding: EdgeInsets.all(2.0),
+              avatar: CircleAvatar(
+                backgroundColor: Colors.redAccent,
+                child: Icon(
+                  _isDishesOn
+                      ? Icons.bakery_dining_rounded
+                      : Icons.bakery_dining_outlined,
+                  color: Colors.white,
+                  size: 20,
+                ),
+              ),
+              label: Text('Plats'),
+              onPressed: () {
+                setState(() {
+                  _isDishesOn = !_isDishesOn;
+                });
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text('Plats')));
+              },
+              backgroundColor: Colors.grey[200],
+              shape: StadiumBorder(
+                  side: BorderSide(
+                width: 1,
+                color: Colors.redAccent,
+              )),
+            ),
+            ActionChip(
+              elevation: 8.0,
+              padding: EdgeInsets.all(2.0),
+              avatar: CircleAvatar(
+                backgroundColor: Colors.yellowAccent,
+                child: Icon(
+                  _isDesertsOn
+                      ? Icons.bakery_dining_rounded
+                      : Icons.bakery_dining_outlined,
+                  color: Colors.white,
+                  size: 20,
+                ),
+              ),
+              label: Text('Desserts'),
+              onPressed: () {
+                setState(() {
+                  _isDesertsOn = !_isDesertsOn;
+                });
+                ScaffoldMessenger.of(context)
+                    .showSnackBar(SnackBar(content: Text('Desserts')));
+              },
+              backgroundColor: Colors.grey[200],
+              shape: StadiumBorder(
+                  side: BorderSide(
+                width: 1,
+                color: Colors.redAccent,
+              )),
+            ),
+          ])),
+      _isSearchEmpty
+          ? Expanded(child: Lottie.asset("36895-healthy-or-junk-food.json"))
+          : Expanded(child: ListView.separated(
+              itemCount: duplicateItems.length,
+              separatorBuilder: (BuildContext context, int index) =>
+                  const Divider(),
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(duplicateItems[index]),
+                  leading: Icon(Icons.favorite),
+                  onTap: () {
+                    // action
+                  },
+                );
+              },
+            ),)
+    ]));
   }
 }
 
