992972a4dd4014b61a705aba9f2eff2c56f9b740
etiamayedze <etiam.ayedze@gmail.com>
2022-02-17 21:48:47 +0100

    organize packages

---
+++ lib/splashScreen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+import '../main.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _navigatetohome();
+  }
+
+  _navigatetohome() async {
+    await Future.delayed(Duration(milliseconds: 1500), () {});
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => MyHomePage(
+              title: 'GFG',
+            )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.asset('assets/images/alcohol.png', height: 200),
+              Text('TchinTchin',
+                  style: TextStyle(
+                      fontSize: 30,
+                      color: const Color(0xff37718E),
+                      fontWeight: FontWeight.bold)),
+            ],
+          )
+      ),
+    );
+  }
+}
