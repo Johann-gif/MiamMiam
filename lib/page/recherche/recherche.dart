1233ccba15d2db588bb484ee81beefc4f8890ab3
denisjunior <denisdalmeida04@gmail.com>
2022-02-19 13:49:48 +0100

    :white_check_mark: add the research

--- lib/page/recherche/recherche.dart
+++ lib/page/recherche/recherche.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:lottie/lottie.dart';
 
 class Recherche extends StatelessWidget {
@@ -6,11 +8,40 @@ class Recherche extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Center(child: Text("Recherche")),
+    return Center(
+      child: Column(children: [
+        //Center(child: Text("Recherche")),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text('Rechercher par ',
+                  style: GoogleFonts.mochiyPopOne(
+                    color: CupertinoColors.black,
+                    fontSize: 15,
+                  )),
+              Text('texte',
+                  style: GoogleFonts.mochiyPopOne(
+                    color: Colors.orange,
+                    fontSize: 15,
+                  )),
+              Text(' ou par ',
+                  style: GoogleFonts.mochiyPopOne(
+                    color: CupertinoColors.black,
+                    fontSize: 15,
+                  )),
+              Text('genre',
+                  style: GoogleFonts.mochiyPopOne(
+                    color: Colors.orange,
+                    fontSize: 15,
+                  ))
+            ],
+          ),
+        ),
         Expanded(child: Lottie.asset("assets/food.json"))
-      ],
+      ],)
     );
   }
 }
