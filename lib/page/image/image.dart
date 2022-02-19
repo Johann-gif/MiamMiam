5b77798b4da07a89b7cb7913762baa7705a5e3fc
tristan <mytic12@hotmail.fr>
2022-02-19 15:49:52 +0100

    add preparation

--- lib/page/image/image.dart
+++ lib/page/image/image.dart
@@ -5,24 +5,25 @@ import 'package:flutter/services.dart';
 import '../../widget/mini_list_item.dart';
 
 class MyImage extends StatelessWidget {
-  const MyImage({Key? key, this.title, this.image, this.description, this.ingredients}) : super(key: key);
+  const MyImage({Key? key, this.title, this.image, this.description, this.ingredients, this.preparation}) : super(key: key);
 
   final image;
   final title;
   final description;
   final ingredients;
+  final preparation;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 
         backgroundColor: Colors.white,
-        body: _scrollImage(image, title, description, ingredients));
+        body: _scrollImage(image, title, description, ingredients, preparation));
 
   }
 
 
-  Widget _scrollImage(String image, String title, String description, List ingredients) {
+  Widget _scrollImage(String image, String title, String description, List ingredients, List preparation) {
     //Plein écran lors de l'affichage de la fenêtre
     //SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
     return NestedScrollView(
@@ -91,6 +92,21 @@ class MyImage extends StatelessWidget {
                   color: const Color(0xfffafafa),
                   child: Text(description))))
           : Container(),
+      Container(
+          child: Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                  padding: const EdgeInsets.only(bottom: 30),
+                  color: const Color(0xfffafafa),
+                  child: Column(children: [
+                    Container(
+                        margin: const EdgeInsets.only(
+                            top: 20, left: 22, right: 22),
+                        child: Wrap(children: [
+                          for (var i = 0; i < preparation.length; i++)
+                            Text(preparation[i])
+                        ]))
+                  ]))))
 
 
     ]))
