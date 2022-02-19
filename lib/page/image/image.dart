99b583355dc252622cce2eecb086c69ca822df33
tristan <mytic12@hotmail.fr>
2022-02-19 16:31:19 +0100

    add ratings

--- lib/page/image/image.dart
+++ lib/page/image/image.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:lottie/lottie.dart';
 
 import '../../widget/mini_list_item.dart';
 
 class MyImage extends StatelessWidget {
-  const MyImage({Key? key, this.title, this.image, this.description, this.ingredients, this.preparation}) : super(key: key);
+  const MyImage({Key? key, this.title, this.image, this.description, this.ingredients, this.preparation, this.rating}) : super(key: key);
 
   final image;
   final title;
   final description;
   final ingredients;
   final preparation;
+  final rating;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
 
         backgroundColor: Colors.white,
-        body: _scrollImage(image, title, description, ingredients, preparation));
+        body: _scrollImage(image, title, description, ingredients, preparation, rating));
 
   }
 
 
-  Widget _scrollImage(String image, String title, String description, List ingredients, List preparation) {
+  Widget _scrollImage(String image, String title, String description, List ingredients, List preparation, int rating) {
     //Plein écran lors de l'affichage de la fenêtre
     //SystemChrome.setEnabledSystemUIMode(SystemUiMode.leanBack);
     return NestedScrollView(
@@ -68,6 +70,21 @@ class MyImage extends StatelessWidget {
                                     : '')
                         ]))
                   ])))),
+      Container(
+          child: Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                  padding: const EdgeInsets.only(bottom: 30),
+                  color: const Color(0xfffafafa),
+                  child: Column(children: [
+                    Container(
+                        margin: const EdgeInsets.only(
+                            top: 20, left: 22, right: 22),
+                        child: Wrap(children: [
+                          for (var i = 0; i < rating; i++)
+                            Lottie.asset("assets/star.json", height: 65, width: 65),
+
+                  ]))])))),
       Container(
           child: Align(
               alignment: Alignment.centerLeft,
